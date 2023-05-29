db8d382050adbbd810ae30878006f18082f70b57
Vladimir Timofeenko <id@vtimofeenko.com>
2023-05-29 16:40:40 -0700

    nix: add home-manager user service

--- nix/home-module.nix
+++ nix/home-module.nix
@@ -3,6 +3,7 @@
 with lib;
 let
   cfg = config.programs.pyprland;
+  serviceCfg = config.services.pyprland;
 in
 {
   options = {
@@ -17,11 +18,36 @@ in
         type = types.attrs;
       };
     };
-  };
-  config = mkIf cfg.enable {
-    home.packages = [ cfg.package ];
-    xdg.configFile."hypr/pyprland.json" = {
-      text = builtins.toJSON cfg.extraConfig;
+    services.pyprland = {
+      enable = mkEnableOption "" // {
+        description = ''
+          Whether to enable the pyprland systemd service.
+          This module is complementary to <code>programs.pyprland</code> which handles the
+          configuration (profiles).
+        '';
+      };
     };
   };
+  config = mkMerge [
+    (mkIf cfg.enable
+      {
+        home.packages = [ cfg.package ];
+        xdg.configFile."hypr/pyprland.json" = {
+          text = builtins.toJSON cfg.extraConfig;
+        };
+      })
+    (mkIf serviceCfg.enable
+      {
+        systemd.user.services.pyprland = {
+          Unit = {
+            Description = "pyprland service";
+            BindsTo = [ "hyprland-session.target" ];
+          };
+          Service = {
+            ExecStart = "${lib.getExe cfg.package}";
+          };
+          Install.WantedBy = [ "graphical-session.target" ];
+        };
+      })
+  ];
 }
