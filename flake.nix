c6ecd76b821a7375387169d223fb3d0337deed82
Vladimir Timofeenko <id@vtimofeenko.com>
2023-05-29 11:05:40 -0700

    Add nix flake
    This commit adds nix flake with pyprland package (available as
    packages.default and in overlays.default).

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    poetry2nix.url = "github:nix-community/poetry2nix";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devshell.url = "github:numtide/devshell";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.flake-parts.flakeModules.easyOverlay
+        inputs.devshell.flakeModule
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        let
+          inherit (inputs.poetry2nix.legacyPackages.${system}) mkPoetryApplication;
+          inherit (inputs.poetry2nix.packages.${system}) poetry;
+        in
+        {
+          # Per-system attributes can be defined here. The self' and inputs'
+          # module parameters provide easy access to attributes of the same
+          # system.
+          #
+          overlayAttrs = {
+            inherit (config.packages) pyprland;
+          };
+          packages = rec {
+            pyprland = mkPoetryApplication { projectDir = ./.; };
+            default = pyprland;
+          };
+          formatter = pkgs.nixpkgs-fmt;
+          devshells.default = {
+            env = [ ];
+            commands = [ ];
+            packages = [
+              poetry
+              pkgs.pre-commit
+            ];
+          };
+        };
+      flake = { };
+    };
+}
